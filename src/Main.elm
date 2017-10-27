85e47de47e911f532df612b32ca9eb41c89aa9a6
Jeff Crocker <crockagile@gmail.com>
2017-10-26 18:18:45 -0700

    Switch to style element layout

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,27 @@
-module Main exposing (..)
+module Main exposing (main)
 
-import Html exposing (Html, text, div, img)
-import Html.Attributes exposing (src)
+import Color
+import Html exposing (Html)
+import Element exposing (..)
+import Element.Attributes exposing (..)
+import Style exposing (..)
+import Style.Color as Color
+import GoogleCivic
 
 
----- MODEL ----
+type alias InputColumn =
+    { text : String
+    }
+
+
+type alias ComputedColumn =
+    { text : String
+    }
+
+
+type Column
+    = Input InputColumn
+    | Computed ComputedColumn
 
 
 type alias Model =
@@ -16,10 +33,6 @@ init =
     ( {}, Cmd.none )
 
 
-
----- UPDATE ----
-
-
 type Msg
     = NoOp
 
@@ -29,20 +42,52 @@ update msg model =
     ( model, Cmd.none )
 
 
+type Styles
+    = None
+    | Page
+    | Header
+    | Table
+    | CsvButton
 
----- VIEW ----
+
+stylesheet : Style.StyleSheet Styles variation
+stylesheet =
+    Style.styleSheet
+        [ style None
+            []
+        , style Page
+            []
+        , style Header
+            []
+        , style Table
+            []
+        , style CsvButton
+            []
+        ]
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , div [] [ text "Your Elm App is working!" ]
+    layout stylesheet <|
+        column Page
+            []
+            [ header model
+            , table model
+            ]
+
+
+header : Model -> Element Styles variation msg
+header model =
+    row Header
+        [ spread, paddingXY 20 5 ]
+        [ el None [] (text "Civics Table")
+        , el None [] (text "Buttons")
         ]
 
 
-
----- PROGRAM ----
+table : Model -> Element Styles variation msg
+table model =
+    el Table [] (text "table here")
 
 
 main : Program Never Model Msg
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -4,9 +4,6 @@ import Test exposing (..)
 import Expect
 
 
--- Check out http://package.elm-lang.org/packages/elm-community/elm-test/latest to learn more about testing in Elm!
-
-
 all : Test
 all =
     describe "A Test Suite"
